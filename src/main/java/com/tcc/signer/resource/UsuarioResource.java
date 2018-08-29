51412d3f9a8592099f9dd5eac1c3c6019caffe14
Bruno Tardio <brunotardiosi@gmail.com>
2018-08-29 19:40:42 -0300

    REST testado

---
+++ src/main/java/com/tcc/signer/resource/UsuarioResource.java
@@ -0,0 +1,18 @@
+package com.tcc.signer.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "usuarios")
+public class UsuarioResource {
+
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST esta ok";
+	}
+	
+	
+}
