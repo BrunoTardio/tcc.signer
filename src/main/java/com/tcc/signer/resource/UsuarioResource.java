310b5ea72e87a78b13d4a7ca77ac336989e6a556
Bruno Tardio <brunotardiosi@gmail.com>
2018-08-29 20:05:37 -0300

    Teste UsuarioDominio

--- src/main/java/com/tcc/signer/resource/UsuarioResource.java
+++ src/main/java/com/tcc/signer/resource/UsuarioResource.java
@@ -1,8 +1,13 @@
 package com.tcc.signer.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tcc.signer.domain.Usuario;
 
 @RestController
 @RequestMapping(value = "usuarios")
@@ -10,8 +15,16 @@ public class UsuarioResource {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta ok";
+	public List<Usuario> listar() {
+		
+		Usuario us1 = new Usuario(1,"Teste","00aa");
+		Usuario us2 = new Usuario(2,"TFFF","00aaff");
+		
+		List<Usuario> lista = new ArrayList<>();
+		lista.add(us1);
+		lista.add(us2);
+		
+		return lista;
 	}
 	
 	
