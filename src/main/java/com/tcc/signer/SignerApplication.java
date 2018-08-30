f4396ac65bd0489d0ddae6389b8d9acecd2e5643
Bruno Tardio <brunotardiosi@gmail.com>
2018-08-29 23:06:08 -0300

    Testando salvar dados ao banco

--- src/main/java/com/tcc/signer/SignerApplication.java
+++ src/main/java/com/tcc/signer/SignerApplication.java
@@ -1,12 +1,32 @@
 package com.tcc.signer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SignerApplication {
+import com.tcc.signer.domain.Usuario;
+import com.tcc.signer.repositories.UsuarioRepository;
 
+@SpringBootApplication
+public class SignerApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SignerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario user1 = new Usuario(null, "tardio@","123");
+		Usuario user2 = new Usuario(null, "xxxxx@","123");
+		Usuario user3 = new Usuario(null, "fffff@","123");
+		
+		usuarioRepository.save(user1);
+		usuarioRepository.save(user2);
+		usuarioRepository.save(user3);
 	}
 }
