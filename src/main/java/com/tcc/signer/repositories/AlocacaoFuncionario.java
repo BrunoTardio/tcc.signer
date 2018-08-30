9d09c5c4846a1870b0d19d1b46f08d9e9395b9ad
Bruno Tardio <brunotardiosi@gmail.com>
2018-08-30 10:19:48 -0300

    AlocacaoFuncionario DOMAIN

---
+++ src/main/java/com/tcc/signer/repositories/AlocacaoFuncionario.java
@@ -0,0 +1,7 @@
+package com.tcc.signer.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlocacaoFuncionario extends JpaRepository<AlocacaoFuncionario, Integer> {
+
+}
