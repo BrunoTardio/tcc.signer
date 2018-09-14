96b800d875f9b9bb8f64b4abfda26e7b1bd24988
Bruno Tardio <brunotardiosi@gmail.com>
2018-09-14 15:07:43 -0300

    DELETE - ProdutoDescricao

--- src/main/java/com/tcc/signer/resources/ProdutoDescricaoResource.java
+++ src/main/java/com/tcc/signer/resources/ProdutoDescricaoResource.java
@@ -46,7 +46,11 @@ public class ProdutoDescricaoResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+	return ResponseEntity.noContent().build();
+	}
 	
 	
 }
--- src/main/java/com/tcc/signer/services/ProdutoDescricaoService.java
+++ src/main/java/com/tcc/signer/services/ProdutoDescricaoService.java
@@ -3,10 +3,12 @@ package com.tcc.signer.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.tcc.signer.domain.ProdutoDescricao;
 import com.tcc.signer.repositories.ProdutoDescricaoRepository;
+import com.tcc.signer.services.exceptions.DataIntegrityException;
 import com.tcc.signer.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,19 +19,27 @@ public class ProdutoDescricaoService {
 
 	public ProdutoDescricao find(Integer id) {
 		Optional<ProdutoDescricao> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException( 
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + ProdutoDescricao.class.getName())); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + ProdutoDescricao.class.getName()));
 	}
-	
+
 	public ProdutoDescricao insert(ProdutoDescricao obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public ProdutoDescricao update(ProdutoDescricao obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Existe Produtos atrelados a este Produto Descricao");
+		}
+	}
 
 }
