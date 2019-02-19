9b7c26577f0663535d3982911af0b599667c235a
BrunoTardio <brunotardiosi@gmail.com>
2019-02-19 19:58:05 -0300

    Autorizacao - teste

--- src/main/java/com/tcc/signer/domain/config/SecurityConfig.java
+++ src/main/java/com/tcc/signer/domain/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.tcc.signer.security.JWTAuthenticationFilter;
+import com.tcc.signer.security.JWTAuthorizationFilter;
 import com.tcc.signer.security.JWTUtil;
 
 
@@ -65,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
